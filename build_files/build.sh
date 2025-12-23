614918dd5a292e3c1f5ef81ab1563fbeb6fb87bb
Mr. Grizzl <160102-mrgrizzl@users.noreply.framagit.org>
2025-12-23 19:35:22 +0000

    First iteration: base-main:43 img with the Niri Window Manager

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,15 +10,16 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y gvfs-afc gvfs-mtp ifuse podman podman-compose micro pass
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+# Use a COPR
+dnf5 -y copr enable avengemedia/dms
+dnf5 -y install niri dms
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable avengemedia/dms
 
-#### Example for enabling a System Unit File
+#### Enabling a User Unit File (globally)
+systemctl --global add-wants niri.service dms
 
+#### Enabling a System Unit File
 systemctl enable podman.socket
