d583041528ac83b091fd1beda977151e82134369
Mr. Grizzl <249494742+mrgrizzl@users.noreply.github.com>
2025-12-24 10:51:46 +0000

    build.sh: test packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,22 +10,19 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 remove -y tmux vim
+dnf5 remove -y tmux vim-X11
 
 # this installs a package from fedora repos
 dnf5 install -y gvfs-afc gvfs-mtp helix ifuse micro pass
 
 # Use a COPR
 dnf5 -y copr enable avengemedia/dms
-dnf5 -y install niri dms dms-greeter
+dnf5 -y install quickshell niri #dms dms-greeter
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable avengemedia/dms
 
-#### Setup DMS Greeter
-dms greeter enable
-
 #### Enabling a User Unit File (globally)
-systemctl --global add-wants niri.service dms
+# systemctl --global add-wants niri.service dms
 
 #### Enabling a System Unit File
 systemctl enable podman.socket
